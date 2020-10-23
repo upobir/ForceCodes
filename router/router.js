32935bd54bb1ea5d84754f6b20a07880f98888c9
upobir <red.abiir@gmail.com>
2020-10-23 11:37:36 +0600

    added template layout for webpage

--- router/router.js
+++ router/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.render('../views/index.ejs');
+    res.render('../views/layout.ejs', {body : 'home'});
 });
 
 module.exports = router
